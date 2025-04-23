44c42d43b3ce88cb15612bc35c53512f05055aff
huepn <phamngochue127@gmail.com>
2025-04-23 18:04:39 +0700

    fix ui login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 // import { useAuth } from '../context/AuthContext'
@@ -14,6 +13,8 @@ import {
   CircularProgress,
 } from '@mui/material'
 
+import { useLocation } from 'react-router-dom'
+
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -24,6 +25,12 @@ const Login = () => {
   const [errorMessage, setErrorMessage] = useState('')
 
   const [errors, setErrors] = useState({ username: '', password: '' })
+
+  const location = useLocation()
+
+  const [redirectUri, setRedirectUri] = useState('')
+
+  const [checkingLogin, setCheckingLogin] = useState(true)
 
   // Hàm validate các trường nhập
   const validate = () => {
@@ -77,6 +84,38 @@ const Login = () => {
     console.log('Google login failed:', error)
   }
 
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const redirect = params.get('redirect_uri')
+    setRedirectUri(redirect)
+
+    const token = localStorage.getItem('access_token')
+    const userName = localStorage.getItem('userName')
+    // Gọi checkLogin
+    setCheckingLogin(true)
+
+    if (token && userName !== null) {
+      setTimeout(() => {
+        axios
+          .post(`http://localhost:8080/api/check`, {
+            token: token,
+          })
+          .then((res) => {
+            if (res.data.status) {
+              const token = res.data.data
+
+              window.location.href =
+                redirect + '?userName=' + userName + '&access_token=' + token
+            } else {
+              setCheckingLogin(false)
+            }
+          })
+      }, 1000)
+    } else {
+      setCheckingLogin(false)
+    }
+  }, [])
+
   const handleSubmit = async (e) => {
     // Validate input, nếu không hợp lệ thì không gọi API
     if (!validate()) return
@@ -88,12 +127,16 @@ const Login = () => {
       })
 
       if (response.data.status) {
-        // login(response.data.data.user, response.data.data.token) // Lưu thông tin người dùng vào AuthContext
+        const searchParams = new URLSearchParams(location.search)
+        const redirectUri = searchParams.get('redirect_uri')
 
-        console.log('response', response.data.data.token)
-
-        // Chuyển hướng tới dashboard hoặc trang chính của ứng dụng
-        navigate('/dashboard')
+        localStorage.setItem('access_token', response.data.data.token)
+        window.location.href =
+          redirectUri +
+          '?userName=' +
+          username +
+          '&access_token=' +
+          response.data.data.token
       } else {
         setErrorMessage(response.data.message || 'Đăng nhập thất bại')
       }
@@ -105,76 +148,88 @@ const Login = () => {
     }
   }
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      bgcolor="#f5f5f5"
-    >
-      <Paper elevation={3} sx={{ p: 4, width: 350 }}>
-        <Typography variant="h5" textAlign="center" mb={3}>
-          Đăng nhập hệ thống
-        </Typography>
+  if (checkingLogin) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <CircularProgress />
+      </Box>
+    )
+  } else {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        bgcolor="#f5f5f5"
+      >
+        <Paper elevation={3} sx={{ p: 4, width: 350 }}>
+          <Typography variant="h5" textAlign="center" mb={3}>
+            Đăng nhập hệ thống
+          </Typography>
 
-        <Stack spacing={2}>
-          <TextField
-            label="Tên đăng nhập"
-            variant="outlined"
-            fullWidth
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value)
-              if (errors.username) {
-                setErrors((prev) => ({ ...prev, username: '' }))
-              }
-            }}
-            error={!!errors.username}
-            helperText={errors.username}
-          />
-          <TextField
-            label="Mật khẩu"
-            variant="outlined"
-            type="password"
-            fullWidth
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value)
-              if (errors.password) {
-                setErrors((prev) => ({ ...prev, password: '' }))
-              }
-            }}
-            error={!!errors.password}
-            helperText={errors.password}
-          />
-          <Button
-            variant="contained"
-            disabled={loading} // Vô hiệu hóa button khi đang loading
-            color="primary"
-            onClick={handleSubmit}
-          >
-            {loading ? (
-              <CircularProgress size={24} color="inherit" />
-            ) : (
-              'Đăng nhập'
+          <Stack spacing={2}>
+            <TextField
+              label="Tên đăng nhập"
+              variant="outlined"
+              fullWidth
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value)
+                if (errors.username) {
+                  setErrors((prev) => ({ ...prev, username: '' }))
+                }
+              }}
+              error={!!errors.username}
+              helperText={errors.username}
+            />
+            <TextField
+              label="Mật khẩu"
+              variant="outlined"
+              type="password"
+              fullWidth
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+                if (errors.password) {
+                  setErrors((prev) => ({ ...prev, password: '' }))
+                }
+              }}
+              error={!!errors.password}
+              helperText={errors.password}
+            />
+            <Button
+              variant="contained"
+              disabled={loading} // Vô hiệu hóa button khi đang loading
+              color="primary"
+              onClick={handleSubmit}
+            >
+              {loading ? (
+                <CircularProgress size={24} color="inherit" />
+              ) : (
+                'Đăng nhập'
+              )}
+            </Button>
+            {errorMessage && (
+              <Typography color="error" fontSize={14} textAlign="center">
+                {errorMessage}
+              </Typography>
             )}
-          </Button>
-          {errorMessage && (
-            <Typography color="error" fontSize={14} textAlign="center">
-              {errorMessage}
-            </Typography>
-          )}
-          {/* <GoogleLogin
+            {/* <GoogleLogin
             onSuccess={handleGoogleLoginSuccess}
             onError={handleGoogleLoginFailure}
             useOneTap // Kích hoạt tính năng đăng nhập một lần (One Tap)
           /> */}
-        </Stack>
-      </Paper>
-    </Box>
-  )
-  
+          </Stack>
+        </Paper>
+      </Box>
+    )
+  }
 }
 
 export default Login
